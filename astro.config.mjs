3ffeaf8855a081489efe9688688a69ea904079cb
Turnip <turnip1202@outlook.com>
2024-12-13 21:55:22 +0800

    build: 更新基础路径以部署到 GitHub Pages
    - 将 Astro 项目的基础路径从 '/my-blog' 修改为 '/my-blog-astro'
    - 此修改是为了适应 GitHub Pages 的部署需求，确保网站能正确显示

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://turnip1202.github.io/',
-  base: '/my-blog',
+  base: '/my-blog-astro',
   trailingSlash: 'always',
   integrations: [
     tailwind({
